7d345827e7f49bf755ad998448887a0053972ce9
= Nfalah25 <= falahnaufal1@gmail.com>
2023-04-22 18:45:47 +0700

    JS-6

---
+++ resources/views/home.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+
+                    <table class="table table-responsive">
+                        <tr>
+                            <th>username</th>
+                            <th>:</th>
+                            <th>{{ $user->username }}</th>
+                        </tr>
+                        <tr>
+                            <th>name</th>
+                            <th>:</th>
+                            <th>{{ $user->name }}</th>
+                        </tr>
+                        <tr>
+                            <th>email</th>
+                            <th>:</th>
+                            <th>{{ $user->email }}</th>
+                        </tr>
+                        <tr>
+                            <th>Create At</th>
+                            <th>:</th>
+                            <th>{{ $user->created_at }}</th>
+                        </tr>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
